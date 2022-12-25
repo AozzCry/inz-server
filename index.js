dc9de3946af8ed89ea0fad0c95dee532b8b63f9e
AozzCry <fajny.car@gmail.com>
2022-12-25 18:35:12 +0100

    deployment prepare

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://emicro.azurewebsites.net"],
+    origin: ["https://emicro.azurewebsites.net"],
     credentials: true,
   })
 );
